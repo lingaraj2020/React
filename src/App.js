4fd5a2c1b71e46330aa03903ba496275a6403684
lingaraj2020 <lingarajhonasani2020@gmail.com>
2023-03-30 13:21:36 +0530

    passing data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
-import ExpenseItem  from './components/ExpenseItem'
-
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location:"tumkur",
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12),location:"delhi" },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location:"mysore",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location:"bombay",
+    },
+  ];
   return (
     <div>
       <h2>lets started</h2>
-      <ExpenseItem/>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+        loc={expenses[0].location}
+      ></ExpenseItem>
+      <ExpenseItem 
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+        loc={expenses[1].location}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+        loc={expenses[2].location}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+        loc={expenses[3].location}
+      ></ExpenseItem>
     </div>
   );
 }
