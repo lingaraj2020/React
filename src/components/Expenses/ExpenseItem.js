3a7e7f2f2e01b7aa27c360bd161bcdb6b3d17f14
lingaraj2020 <lingarajhonasani2020@gmail.com>
2023-04-18 16:39:23 +0530

    dom manipulation-react state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseDate.css";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseDetails.css";
@@ -7,17 +7,25 @@ import "./ExpenseDetails.css";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const clickHandler=()=>{
-    console.log("clicked!...");
-  }
+  const [title, setTitle] = useState(setTitle.title);
+  console.log("Expense item evaluated by react");
+
+  const clickHandler = () => {
+    setTitle("updated!..");
+
+    console.log(title);
+  };
+
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button className="btn-click" onClick={clickHandler}>Delete Expense</button>
+      <button className="btn-click" onClick={clickHandler}>
+        Delete Expense
+      </button>
     </div>
   );
 };
